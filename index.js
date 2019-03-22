ae3eb8f8566d690ec176aee2004cb90d59dd2d5e
asafos <yehezkel6@gmail.com>
2019-03-22 11:28:24 +0200

    Changed favicon

--- index.js
+++ index.js
@@ -5,6 +5,15 @@ import {Provider} from "react-redux";
 import { store, history } from "./src/redux/store";
 import './css/styles.css';
 import { ConnectedRouter } from 'connected-react-router';
+import Favicon from './assets/images/favicon.png';
+
+(function() {
+    var link = document.querySelector("link[rel*='icon']") || document.createElement('link');
+    link.type = 'image/x-icon';
+    link.rel = 'shortcut icon';
+    link.href = Favicon;
+    document.getElementsByTagName('head')[0].appendChild(link);
+})();
 
 render(<Provider store={store}>
     <ConnectedRouter history={history}>
